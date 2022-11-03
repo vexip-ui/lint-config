73a43db1f55b074f25413c6c3990cb5803f1e5d9
qmhc <40221744+qmhc@users.noreply.github.com>
2022-11-03 17:31:00 +0800

    fix(eslint-config): improve @typescript-eslint/indent

--- packages/eslint-config/index.cjs
+++ packages/eslint-config/index.cjs
@@ -55,7 +55,7 @@ module.exports = defineConfig({
   },
   parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 'latest'
   },
   reportUnusedDisableDirectives: true,
   settings: {
@@ -69,7 +69,7 @@ module.exports = defineConfig({
           '.js',
           '.jsx',
           '.json',
-          '.node',
+          '.node'
         ]
       }
     }
@@ -91,6 +91,44 @@ module.exports = defineConfig({
     'no-return-assign': 'off',
 
     // typescript
+    '@typescript-eslint/indent': ['error', 2, {
+      SwitchCase: 1,
+      VariableDeclarator: 1,
+      outerIIFEBody: 1,
+      MemberExpression: 1,
+      FunctionDeclaration: { parameters: 1, body: 1 },
+      FunctionExpression: { parameters: 1, body: 1 },
+      CallExpression: { arguments: 1 },
+      ArrayExpression: 1,
+      ObjectExpression: 1,
+      ImportDeclaration: 1,
+      flatTernaryExpressions: false,
+      ignoreComments: false,
+      ignoredNodes: [
+        'TemplateLiteral *',
+        'JSXElement',
+        'JSXElement > *',
+        'JSXAttribute',
+        'JSXIdentifier',
+        'JSXNamespacedName',
+        'JSXMemberExpression',
+        'JSXSpreadAttribute',
+        'JSXExpressionContainer',
+        'JSXOpeningElement',
+        'JSXClosingElement',
+        'JSXFragment',
+        'JSXOpeningFragment',
+        'JSXClosingFragment',
+        'JSXText',
+        'JSXEmptyExpression',
+        'JSXSpreadChild',
+        'TSTypeParameterInstantiation',
+        'FunctionExpression > .params[decorators.length > 0]',
+        'FunctionExpression > .params > :matches(Decorator, :not(:first-child))',
+        'ClassBody.body > PropertyDefinition[decorators.length > 0] > .key'
+      ],
+      offsetTernaryExpressions: true
+    }],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/space-before-function-paren': [
