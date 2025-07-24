3c76ae22ec8194a6f6479b37d76fde06e373cb99
qmhc <rocco@moego.pet>
2025-07-24 11:54:06 +0800

    fix(eslint-config): adjust @typescript-eslint/no-unused-expressions

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -146,48 +146,7 @@ function factory({ ignores = [] }: FactoryOptions = {}) {
           }
         ],
 
-        '@stylistic/indent': [
-          'error',
-          2,
-          {
-            SwitchCase: 1,
-            VariableDeclarator: 1,
-            outerIIFEBody: 1,
-            MemberExpression: 1,
-            FunctionDeclaration: { parameters: 1, body: 1 },
-            FunctionExpression: { parameters: 1, body: 1 },
-            CallExpression: { arguments: 1 },
-            ArrayExpression: 1,
-            ObjectExpression: 1,
-            ImportDeclaration: 1,
-            flatTernaryExpressions: false,
-            ignoreComments: false,
-            ignoredNodes: [
-              'TemplateLiteral *',
-              'JSXElement',
-              'JSXElement > *',
-              'JSXAttribute',
-              'JSXIdentifier',
-              'JSXNamespacedName',
-              'JSXMemberExpression',
-              'JSXSpreadAttribute',
-              'JSXExpressionContainer',
-              'JSXOpeningElement',
-              'JSXClosingElement',
-              'JSXFragment',
-              'JSXOpeningFragment',
-              'JSXClosingFragment',
-              'JSXText',
-              'JSXEmptyExpression',
-              'JSXSpreadChild',
-              'TSTypeParameterInstantiation',
-              'FunctionExpression > .params[decorators.length > 0]',
-              'FunctionExpression > .params > :matches(Decorator, :not(:first-child))',
-              'ClassBody.body > PropertyDefinition[decorators.length > 0] > .key'
-            ],
-            offsetTernaryExpressions: true
-          }
-        ],
+        '@stylistic/indent': ['error', 2],
         '@stylistic/space-before-function-paren': [
           'error',
           {
@@ -251,6 +210,14 @@ function factory({ ignores = [] }: FactoryOptions = {}) {
             argsIgnorePattern: '^_',
             caughtErrors: 'none',
             ignoreRestSiblings: true
+          }
+        ],
+        '@typescript-eslint/no-unused-expressions': [
+          'error',
+          {
+            allowShortCircuit: true,
+            allowTernary: true,
+            allowTaggedTemplates: true,
           }
         ],
 
